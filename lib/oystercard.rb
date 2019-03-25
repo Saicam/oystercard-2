b9f3699fc3a3d040c73987219e13451398b815e7
Jose Macias <josemariamacias@gmail.com>
2019-03-25 15:11:25 +0000

    Added balance and initialize method to OysterCard, also created it unit test

---
+++ lib/oystercard.rb
@@ -0,0 +1,7 @@
+class OysterCard
+  def initialize
+    @balance = 0
+  end
+
+  attr_reader :balance
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require "oystercard.rb"
+
 describe OysterCard do
-  
+
+  describe "#initialize" do
+    it "set a balance of zero when initializing" do
+      expect(subject.balance).to eq 0
+    end
+  end
+
 end
